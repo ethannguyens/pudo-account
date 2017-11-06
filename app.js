7b72e0c612811dc3194bed7f213534c9fca3ef91
ethan.nguyen <ethan.nguyens@gmail.com>
2017-11-06 10:39:36 +0000

    add POST account

---
+++ app.js
@@ -0,0 +1,51 @@
+const Couchbase = require("couchbase");
+const Express = require("express");
+const UUID = require("uuid");
+const BodyParser = require("body-parser");
+const Bcrypt = require("bcryptjs");
+
+const app = Express();
+const N1qlQuery = Couchbase.N1qlQuery;
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+const cluster = new Couchbase.Cluster("couchbase://localhost");
+const bucket = cluster.openBucket("default", "");
+
+const server = app.listen(3000, () => {
+  console.log("Listening on port " + server.address().port + "...");
+});
+
+//POST /account creating – Create a new user profile with account information
+app.post("/account", (request, response) => {
+  if(!request.body.email) return response.status(401).send({ "message": "An `email` is required" });
+  else if(!request.body.password) return response.status(401).send({ "message": "A `password` is required" });
+
+  const id = UUID.v4();
+  const account = {
+    "type": "account",
+    "pid": id,
+    "email": request.body.email,
+    "password": Bcrypt.hashSync(request.body.password, 10)
+  };
+
+  const profile = request.body;
+
+  profile.type = "profile";
+
+  delete profile.password;
+
+  bucket.insert(id, profile, (error, result) => {
+    if(error) {
+      return response.status(500).send(error);
+    }
+    bucket.insert(request.body.email, account, (error, result) => {
+      if(error) {
+        bucket.remove(id);
+        return response.status(500).send(error);
+      }
+      response.send(result);
+    });
+  });
+});
